32ccf6e96a7a61370a118dec6a522dc37ea7d96b
Rhys Morgan <rhysm@me.com>
2026-04-06 15:24:14 +0100

    Initial Commit

---
+++ NonIsolatedViewModels/NonIsolatedViewModelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NonIsolatedViewModelsApp.swift
+//  NonIsolatedViewModels
+//
+//  Created by Rhys Morgan on 06/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct NonIsolatedViewModelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
